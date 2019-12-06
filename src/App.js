c249ee577a4bd01011dce73ebc3fdfea9e837497
Jason Campbell <jlcampbell86@gmail.com>
2019-12-06 01:17:00 -0500

    started login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  Grid,
+  Hidden,
+  Paper
+} from '@material-ui/core'
 
 function App() {
+  // let styles = {
+  //   image: {
+  //     backgroundImage: 'url(./images/intro_pair.jpg)',
+  //     backgroundRepeat: 'no-repeat',
+  //     backgroundSize: 'cover',
+  //     backgroundPosition: 'center',
+  //   }
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Grid container component='main'>
+          <Hidden mdDown>
+            <Grid item lg={6} className='intro-img' />
+          </Hidden>
+          <Grid item lg={6}>
+          </Grid>
+        </Grid>
+      </div>
   );
 }
 
---
+++ src/themes.js
@@ -0,0 +1,28 @@
+// import {
+//   ThemeProvider,
+//   createMuiTheme,
+//   responsiveFontSizes
+// } from 'material-ui/core'
+//
+// let mainTheme = createMuiTheme({
+//   palette: {
+//     primary: {
+//       light: '#D1C4E9',
+//       main: '#673AB7',
+//       dark: '#512DA8',
+//       contrastText: getContrastText('#673AB7'),
+//     },
+//     secondary: {
+//       light: palette.secondary.A200,
+//       main: '#FF9800',
+//       dark: palette.secondary.A700,
+//       contrastText: getContrastText(palette.secondary.A400),
+//     },
+//     error: {
+//       light: palette.error[300],
+//       main: palette.error[500],
+//       dark: palette.error[700],
+//       contrastText: getContrastText(palette.error[500]),
+//     },
+//   }
+// })
