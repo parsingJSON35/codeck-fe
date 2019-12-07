9d29bf02e6564dd64c57b564deafc099114c16ba
Jason Campbell <jlcampbell86@gmail.com>
2019-12-07 00:30:47 -0500

    added background, title, form to login page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import './App.css';
 import {
   Grid,
+  Paper,
   Hidden,
+  TextField,
+  Button
 } from '@material-ui/core'
 
 function App() {
@@ -18,10 +21,19 @@ function App() {
   return (
       <div className="App">
         <Grid container component='main'>
-          <Hidden mdDown>
-            <Grid item lg={6} className='intro-img' />
+          <Hidden xsDown>
+            <Grid item xs={false} sm={5} md={7} className='intro-img'>
+              <div id='logo'>
+                <div>Codeck</div>
+              </div>
+            </Grid>
           </Hidden>
-          <Grid item lg={6}>
+          <Grid item xs={12} sm={7} md={5} component={Paper}>
+            <form id='login-form'>
+              <TextField variant='outlined' label='Username' placeholder='Username' required autofocus fullWidth></TextField>
+              <TextField variant='outlined' label='Password' placeholder='Password' required autofocus fullWidth></TextField>
+              <Button fullWidth contained>Sign In</Button>
+            </form>
           </Grid>
         </Grid>
       </div>
